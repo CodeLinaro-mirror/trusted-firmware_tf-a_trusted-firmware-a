f2a1496e40a209cac97bc6dacc9afff451528b4b
Harikrishna <hart@qti.qualcomm.com>
2026-04-21 11:01:12 +0530

    feat(ipq52xx): Aligning the RNG HW registers EE value to PBL (EE5)
    Change-Id: Icdbe7ea032015d9abc0d77c06dc87c5f26b7bae7
    Signed-off-by: Harikrishna <hart@qti.qualcomm.com>

--- plat/qti/ipq52xx/inc/qti_rng_io.h
+++ plat/qti/ipq52xx/inc/qti_rng_io.h
@@ -11,10 +11,9 @@
  * HERMOSA (IPQ5200) QRNG Hardware Register Addresses
  * These registers are mapped to EE5 currently aligning
  * with PBL
- * FIXME: To check which EE is used by TZ
  */
-#define SEC_PRNG_STATUS			0x4C6004
+#define SEC_PRNG_STATUS			0x4C5004
 #define SEC_PRNG_STATUS_DATA_AVAIL_BMSK	0x1
-#define SEC_PRNG_DATA_OUT		0x4C6000
+#define SEC_PRNG_DATA_OUT		0x4C5000
 
 #endif /* QTI_RNG_IO_H */
